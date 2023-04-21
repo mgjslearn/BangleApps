8f9fe077af44130804234c2d07466578131a9fba
mgjslearn <101474281+mgjslearn@users.noreply.github.com>
2023-04-20 23:26:55 -0400

    Update userpf.js

--- PuckTest/ugawatchwebsite/userpf.js
+++ PuckTest/ugawatchwebsite/userpf.js
@@ -1,5 +1,5 @@
  
-
+/*
       $(function() {
         // Initialize date picker
         $('#date-picker').flatpickr({
@@ -34,5 +34,58 @@
         };
         var chart = new ApexCharts(document.querySelector("#activity-graph"), options);
         chart.render();
-      });
+      }); 
+      */
+// Hard-coded example data for steps walked
+const stepsData = {
+    "2023-04-01": 5000,
+    "2023-04-02": 7000,
+    "2023-04-03": 9000,
+    "2023-04-04": 6000,
+    "2023-04-05": 8000,
+    "2023-04-06": 10000,
+    "2023-04-07": 12000
+};
+
+// Function to generate the activity line graph
+function generateActivityGraph(startDate, endDate) {
+    // Filter the steps data based on the selected date range
+    const filteredStepsData = {};
+    const currentDate = new Date(startDate);
+    while (currentDate <= new Date(endDate)) {
+        const dateString = currentDate.toISOString().slice(0, 10);
+        filteredStepsData[dateString] = stepsData[dateString] || 0;
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    // Create an array of dates and an array of steps
+    const dates = Object.keys(filteredStepsData);
+    const steps = Object.values(filteredStepsData);
+
+    // Create the chart options
+    const chartOptions = {
+        chart: {
+            type: 'line'
+        },
+        series: [{
+            name: 'Steps Walked',
+            data: steps
+        }],
+        xaxis: {
+            categories: dates,
+            type: 'datetime'
+        },
+        responsive: [{
+            breakpoint: 576,
+            options: {
+                chart: {
+                    height: 250
+                }
+            }
+        }]
+    };
+
+    // Create the chart
+    const chart = new ApexCharts(document.getElementById('activity-graph'), chartOptions);
+   
 
